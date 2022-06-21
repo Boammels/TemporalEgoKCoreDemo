61385195e0f248b83f87c65d768c0532377ca870
root <root@Boammels.localdomain>
2022-06-21 17:53:38 +1000

    frontend progress

--- frontend/src/pages/Graph.jsx
+++ frontend/src/pages/Graph.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import CytoscapeComponent from 'react-cytoscapejs';
 import { useNavigate, useParams } from 'react-router-dom';
-import '../App.css';
+import './Graph.css';
 import axios from 'axios';
 
 const Graph = () => {
   const [success, setSuccess] = React.useState(false);
   const [elements, setElement] = React.useState([]);
+  const [center, setCenter] = React.useState('')
   const id = useParams().id;
   const start = parseInt(useParams().start);
   const end = parseInt(useParams().end);
@@ -25,6 +26,7 @@ const Graph = () => {
         }
       }).then(({ data }) => {
         setElement(data.elements)
+        setCenter(data.author)
         setSuccess(true)
       }
     )
@@ -37,7 +39,7 @@ const Graph = () => {
   React.useEffect(() => myfetch, [load])
 
   return <>
-    {success && <CytoscapeComponent
+    {success && elements.length !== 0 && <CytoscapeComponent
       elements={elements}
       style={{width: '100vw', height: 'calc(100vh - 50px)'}}
       stylesheet={[
@@ -62,6 +64,17 @@ const Graph = () => {
         name:'circle'
       }}
     />}
+    {success && elements.length === 0 && <div className='main'>
+      <p className='line'>There isn't a <b>{k}</b>-core subgraph</p>
+      <p className='line'>for <u><b>{center}</b></u> from <b>{start}</b> to <b>{end}</b>.</p>
+      <button
+        className='button'
+        onClick={() => navigate('/')}
+      >Search for others</button>
+    </div>}
+    {!success && <div className='main'>
+      <p className='line'><b>Loading......</b></p>
+    </div>}
   </>
 }
 
--- frontend/src/pages/NameList.jsx
+++ frontend/src/pages/NameList.jsx
@@ -9,10 +9,10 @@ const NameList = () => {
   const [k, setK] = React.useState(parseInt(useParams().k)) ;
   const [names, setNames] = React.useState([]);
   const [success, setSuccess] = React.useState(false);
-  const name = useParams().name;
+  const [name, setName] = React.useState(useParams().name);
   const navigate = useNavigate();
-  const load = 1;
-
+  let load = 1;
+  const searchName = useParams().name
   const myfetch = async () => { 
     axios.get('http://127.0.0.1:5000/name/',
       { 
@@ -32,6 +32,22 @@ const NameList = () => {
   }
   // eslint-disable-next-line
   React.useEffect(() => myfetch, [load])
+
+  const submit = () => {
+    setSuccess(false)
+    if (name === '') {
+      alert('Alert: name cannot be empty!');
+    } else if (isNaN(parseInt(start)) || isNaN(parseInt(end))) {
+      alert('year is not number')
+    } else if (parseInt(start) < 1938 || parseInt(end) > 2023) {
+      alert('year query out of bound')
+    } else if (parseInt(start) > parseInt(end)) {
+      alert('start year should earlier or equal to end year')
+    } else {
+      navigate('/name/'+name+'/'+parseInt(start)+'/'+parseInt(end)+'/'+k)
+      window.location.reload()
+    }
+  }
 
   return (
     <><div className='main'>
@@ -60,13 +76,28 @@ const NameList = () => {
             className='btn-sml'
             onClick={() => {k < 20 ? setK(k+1) : setK(k)}}
           >+</button>
+          <input
+            type='text'
+            value={name}
+            className='Input2'
+            placeholder='Name of the researcher'
+            onChange={e => setName(e.target.value)}
+          />
+          <button
+            className='btn-medium1'
+            onClick={() => submit()}
+          >Search</button>
+          <button
+            className='btn-medium2'
+            onClick={() => navigate('')}  
+          >Cancel</button>
         </div>
         <div>
           {success && names.map((name) => {
             return (<>
               <a
                 key={name.id}
-                href={'id/'+name.id +'/'+start+'/'+end+'/'+k}
+                href={'http://localhost:3000/id/'+name.id +'/'+start+'/'+end+'/'+k}
               >
                 <div className='nameListItem'>
                   <p>{name.name}</p>
@@ -75,6 +106,8 @@ const NameList = () => {
             </>);
           })}
         </div>
+        {names.length === 0 && success && <p className='line'>No results for '{searchName}'</p>}
+        {names.length === 0 && !success && <p className='line'>Loading......</p>}
         <div style={{height:'100px', width:'100%'}}></div>
       </div>
     </>
